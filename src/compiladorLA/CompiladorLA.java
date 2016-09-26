13793a2e97788bb4db23a03dab676c41d7aed5a3
LucasBocanegra <lucasfernandobocanegra@gmail.com>
2016-09-25 22:13:59 -0300

    change project directory

--- src/compiladorLA/CompiladorLA.java
+++ src/compiladorLA/CompiladorLA.java
@@ -12,7 +12,7 @@ public class CompiladorLA {
          // Descomente as linhas abaixo para testar o analisador gerado.
          // Obs: a linha abaixo está configurada para usar como entrada o arquivo lua1.txt   
          // Modifique-a para testar os demais exemplos
-         ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("/home/lucas/Documents/bcc/2016/2sem/CC2/Compiladores2/compiladorLA/src/compiladorLA/exemplos/lua1.txt"));
+         ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("/home/lucas/Documents/bcc/2016/2sem/CC2/Compiladores2/src/compiladorLA/exemplos/lua1.txt"));
          ExpressoesLexer lexer = new ExpressoesLexer(input);
          CommonTokenStream tokens = new CommonTokenStream(lexer);
          ExpressoesParser parser = new ExpressoesParser(tokens);
